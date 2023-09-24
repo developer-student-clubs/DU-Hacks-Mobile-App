98b7f33d5b7afa8d3dd071b104fc5b49aae438bc
Om Dhingani <omdhingani0@gmail.com>
2023-09-24 22:43:21 +0530

    Removing Unwanted

--- android/app/src/main/kotlin/com/example/gdsc_ui_design/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gdsc_ui_design
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/showSnackBar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(text),
-    ),
-  );
-}
