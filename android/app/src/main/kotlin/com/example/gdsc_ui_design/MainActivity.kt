abf98bc5aae1cdccd059957570f6abf2840f5675
Om Dhingani <omdhingani0@gmail.com>
2023-09-24 22:46:57 +0530

    DUHacks Design

---
+++ android/app/src/main/kotlin/com/example/gdsc_ui_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gdsc_ui_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/OmUI/RegisteredPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisteredPage extends StatefulWidget {
+  const RegisteredPage({super.key});
+
+  @override
+  State<RegisteredPage> createState() => _RegisteredPageState();
+}
+
+class _RegisteredPageState extends State<RegisteredPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
