c7ad53b11367d782425653ee2125c38288452e97
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2024-01-24 22:47:22 +0530

    a new qr is generating in mark presence screen

--- lib/Screens/OmUI/mark_presence_screen.dart
+++ lib/Screens/OmUI/mark_presence_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class MarkPresence extends StatelessWidget {
+class MarkPresence extends StatefulWidget {
   const MarkPresence({super.key});
 
   @override
+  State<MarkPresence> createState() => _MarkPresenceState();
+}
+
+class _MarkPresenceState extends State<MarkPresence> {
+  String qrData = 'A sample QR code';
+  TextEditingController textFieldController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextField(
+                  controller: textFieldController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter your text',
+                    hintText: 'Type here',
+                    // prefixIcon: Icon(Icons.text_fields),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      qrData = textFieldController.text;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromHeight(50.0)),
+                  child: const Text('Generate QR Code'),
+                ),
+                const SizedBox(height: 26.0),
+                QrImageView(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 320,
+                  gapless: false,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
